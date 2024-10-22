72755114d1476c5c23ce6457fd70f9001d38000f
Caio Thomaz Nogueira <104398804+caio-thom@users.noreply.github.com>
2024-10-22 20:51:03 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Trabalho FIAP 21/10/2024"
+    return "Trabalho FIAP - Core Devops"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
